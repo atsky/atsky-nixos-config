d28c353e0cb515107f9a999822d73717d64585dc
atsky <kurbatsky@gmail.com>
2024-11-05 19:08:20 +0300

    Update

--- configuration.nix
+++ configuration.nix
@@ -4,9 +4,10 @@
 
 { config, pkgs, ... }:
 
+let
+  unstable = import <unstable> { config = { allowUnfree = true; }; };
 
 # Here goes some basic flags
-let
   use_virtualbox = true;
 in {
   imports =
@@ -48,24 +49,59 @@ in {
   # Enable the GNOME 3 Desktop Environment.
   services.xserver = {
     enable = true;
-    displayManager = {
-      gdm.enable = false;
-      autoLogin.enable = true;
-      autoLogin.user = "atsky";
-      defaultSession = "gnome";
-    };
-    
+    displayManager.gdm.enable = true;
+
     #desktopManager.plasma5.enable = true;
     desktopManager.gnome.enable = true;
     #desktopManager.xfce.enable = true;
     videoDrivers = [ "nvidia" ];
   };
 
-  hardware.opengl.driSupport32Bit = true;
-  
+  services.displayManager = {
+    autoLogin.enable = true;
+    autoLogin.user = "atsky";
+    defaultSession = "gnome";
+  };
+ 
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  }; 
 
   # Configure keymap in X11
-  services.xserver.layout = "us,ru";
+  services.xserver.xkb.layout = "us,ru";
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
@@ -86,7 +122,8 @@ in {
   };
 
   nixpkgs.config.permittedInsecurePackages = [
-	"ffmpeg-3.4.8"
+  	"ffmpeg-3.4.8"
+        "electron-25.9.0"
   ]; # For processing  
 
   virtualisation.docker.enable = true;
@@ -95,26 +132,24 @@ in {
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      wget vim git mc htop pciutils usbutils glxinfo 
+     lazygit
      fish zsh
      bitwarden bitwarden-cli
      wireguard-tools
      lsof
-     hfsprogs exfatprogs gparted
+     gparted
      unrar p7zip
      gnumake gcc
-     firefox dropbox gimp inkscape dosbox yandex-disk
+     firefox dropbox gimp inkscape dosbox unstable.yandex-disk
      jdk 
      sublime
-     ghc
      tdesktop
      docker
      # Programming
      python3 jupyter
-     cudatoolkit
      obsidian
-     libusb # for Digispark
      # Latex
-     texlive.combined.scheme-full texmaker
+     # texlive.combined.scheme-full texmaker
   ];
 
   programs.steam.enable = true;
@@ -148,11 +183,11 @@ in {
 	enableExtensionPack = true;
   };
   
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     source-code-pro    
     fira-code
     fira-code-symbols
-    mplus-outline-fonts
+    mplus-outline-fonts.githubRelease
     dina-font
     proggyfonts
   ];
